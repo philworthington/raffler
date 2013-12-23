45eaffb368b6bae7c043b04958193ce365e972f7
Phil Worthington <pworthington27@gmail.com>
2013-12-22 23:01:03 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Raffler
+
+
+By [Phil Worthington](https://github.com/philworthington).
+
+
+## Description
+
+
+## Authors
+
+* Phil Worthington (https://github.com/philworthington)
+
+
+## License
+
+MIT: http://philworthington.mit-license.org
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raffler::Application.config.secret_key_base = 'b1b2c043ad2156b8b478ce17553ed9983fd4d570c34278f6ba205bc7fd58685352ab5f6a12ae66c5fd31955d1dc28d91c2d81fded45d16e07f08eb88cd6c523d'
